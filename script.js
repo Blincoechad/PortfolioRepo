d54a914938f4cf711e12ab363e1472924b787672
blincoechad <blincoechad@gmail.com>
2026-01-07 13:09:52 -0500

    worked on graphic work page

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ document.addEventListener('DOMContentLoaded', function() {
         navLinks.classList.toggle('active');
     });
 });
+
+
+// Get elements
+const overlay = document.getElementById("overlay");
+const overlayImg = document.getElementById("overlay-img");
+const closeBtn = document.getElementById("close");
+
+document.querySelectorAll(".clickable-image").forEach(img => {
+  img.addEventListener("click", () => {
+    overlay.style.display = "flex";
+    overlayImg.src = img.src; // use the same image in overlay
+  });
+});
+
+// Close overlay
+closeBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
+
+//click outside the image closes overlay
+overlay.addEventListener("click", e => {
+  if (e.target === overlay) {
+    overlay.style.display = "none";
+  }
+});
+
